6cdb6ea124daaf5974e7a4678454d4c98372cb2b
Gibran Amparan <gibranamparand@outlook.com>
2022-11-13 17:23:36 -0600

    format code in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'url'
 import VueI18nPlugin from '@intlify/vite-plugin-vue-i18n'
 
-export default ({ mode } : any) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+export default ({ mode }: any) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   const GRAPHQL_URL = process.env.VITE_GRAPHQL_URL
   const KEYCLOAK_URL = process.env.VITE_KEYCLOAK_URL
 
@@ -24,12 +24,12 @@ export default ({ mode } : any) => {
     server: {
       host: true,
       proxy: {
-        "/graphql": {
+        '/graphql': {
           target: GRAPHQL_URL,
           changeOrigin: true,
           secure: false,
         },
-        "/auth": {
+        '/auth': {
           target: KEYCLOAK_URL,
           changeOrigin: true,
           secure: false,
